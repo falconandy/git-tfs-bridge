dcb70435a3ad919a09351286262bf4d77fe0e4a1
Andrey Sokolov <falconandy@yandex.ru>
2015-11-05 17:13:43 +0400

    compare directories

--- tfs_test.go
+++ tfs_test.go
@@ -4,12 +4,54 @@ import (
 	"testing"
 	bridge "github.com/falconandy/git-tfs-bridge"
 	"log"
+	"github.com/sabhiram/go-git-ignore"
 )
 
 func TestHistory(t *testing.T) {
 	tfs := bridge.OpenTfsRepository(`D:\Projects\Sungero\Main\Common`)
 	log.Println(tfs.GetHistoryAfter(98000))
 	log.Println(tfs.GetHistory(50000, 100))
+}
+
+func TestUpdate(t *testing.T) {
+	tfs := bridge.OpenTfsRepository(`D:\Projects\Sungero\Main\Common`)
 	tfs.Update(95000)
 	tfs.Update(0)
+}
+
+func TestTraverseDirectory(t *testing.T) {
+	gitIgnore, err := ignore.CompileIgnoreFile(`D:\Projects\_sun\Main\.gitignore`)
+	if err != nil {
+		log.Println(err)
+	} else {
+		bridge.TraverseDirectory(`D:\Projects\_sun\Main\`, gitIgnore)
+	}
+}
+
+func TestCompareDirectory(t *testing.T) {
+	gitIgnore, _ := ignore.CompileIgnoreFile(`D:\Projects\_sun\Main\.gitignore`)
+	leftOnly, rightOnly, diffs := bridge.CompareDirectories(`D:\Projects\_sun\Main\Common`, `D:\Projects\Sungero\Main\Common`, gitIgnore)
+	log.Println(leftOnly)
+	log.Println(rightOnly)
+	log.Println(diffs)
+	leftOnly, rightOnly, diffs = bridge.CompareDirectories(`D:\Projects\_sun\Main\Kernel`, `D:\Projects\Sungero\Main\Kernel`, gitIgnore)
+	log.Println(leftOnly)
+	log.Println(rightOnly)
+	log.Println(diffs)
+	leftOnly, rightOnly, diffs = bridge.CompareDirectories(`D:\Projects\_sun\Main\Content`, `D:\Projects\Sungero\Main\Content`, gitIgnore)
+	log.Println(leftOnly)
+	log.Println(rightOnly)
+	log.Println(diffs)
+	leftOnly, rightOnly, diffs = bridge.CompareDirectories(`D:\Projects\_sun\Main\Report`, `D:\Projects\Sungero\Main\Report`, gitIgnore)
+	log.Println(leftOnly)
+	log.Println(rightOnly)
+	log.Println(diffs)
+	leftOnly, rightOnly, diffs = bridge.CompareDirectories(`D:\Projects\_sun\Main\Workflow`, `D:\Projects\Sungero\Main\Workflow`, gitIgnore)
+	log.Println(leftOnly)
+	log.Println(rightOnly)
+	log.Println(diffs)
+	leftOnly, rightOnly, diffs = bridge.CompareDirectories(`D:\Projects\_sun\Main\SDS`, `D:\Projects\Sungero\Main\SDS`, gitIgnore)
+	log.Println(leftOnly)
+	log.Println(rightOnly)
+	log.Println(diffs)
 }
--- utils.go
+++ utils.go
@@ -3,6 +3,12 @@ package git_tfs_bridge
 import (
 	"time"
 	"strings"
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"os"
+	"github.com/sabhiram/go-git-ignore"
 )
 
 var russian2englishMonths = map[string]string {
@@ -29,4 +35,63 @@ func parseMaybeRussianDate(layout string, value string) (time.Time, error) {
 		}
 	}
 	return time.Parse(layout, value)
+}
+
+func TraverseDirectory(root string, gitIgnore *ignore.GitIgnore) map[string]string {
+	traverseInfo := make(map[string]string)
+	filepath.Walk(root, func (path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.Name() == ".git" || info.Name() == ".gitignore" || info.Name() == ".hg" || info.Name() == ".hgignore" {
+			if info.IsDir() {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+		relativePath := strings.TrimLeft(strings.TrimPrefix(path, root), string(filepath.Separator))
+		if info.IsDir() {
+			relativePath += string(filepath.Separator)
+		}
+		if gitIgnore.MatchesPath(strings.Replace(relativePath, string(filepath.Separator), "/", -1)) {
+			if info.IsDir() {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+		if !info.IsDir() {
+			content, err := ioutil.ReadFile(path)
+			if err == nil {
+				traverseInfo[relativePath] = fmt.Sprintf("%x", md5.Sum(content))
+			} else {
+				traverseInfo[relativePath] = "ERROR"
+			}
+		}
+		return nil
+	})
+	return traverseInfo
+}
+
+func CompareDirectories(leftPath string, rightPath string, gitIgnore *ignore.GitIgnore) (leftOnly map[string]struct{}, rightOnly map[string]struct{}, diffs map[string]struct{}) {
+	left := TraverseDirectory(leftPath, gitIgnore)
+	right := TraverseDirectory(rightPath, gitIgnore)
+	leftOnly = make(map[string]struct{})
+	rightOnly = make(map[string]struct{})
+	diffs = make(map[string]struct{})
+	for leftPath, leftHash := range left {
+		if rightHash, ok := right[leftPath]; ok {
+			if rightHash != leftHash {
+				diffs[leftPath] = struct{}{}
+			}
+			delete(left, leftPath)
+			delete(right, leftPath)
+		} else {
+			leftOnly[leftPath] = struct{}{}
+			delete(left, leftPath)
+		}
+	}
+	for rightPath, _ := range right {
+		rightOnly[rightPath] = struct{}{}
+	}
+	return leftOnly, rightOnly, diffs
 }
